54ffa00a2011e39c2ed05f338e5dd4ba61f5c1a0
kumarshyam5 <kumarshyam5@gmail.com>
2016-01-05 02:57:36 -0600

    Added Login View

---
+++ MRP4u_Sample/MRP4u_Sample/LoginView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace MRP4u_Sample
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : MetroWindow
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow mn = new MainWindow();
+            mn.Show();
+            this.Close();
+        }
+    }
+}
