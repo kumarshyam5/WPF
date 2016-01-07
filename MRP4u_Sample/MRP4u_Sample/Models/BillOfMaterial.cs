23fd3f6035885eab0d9fff4dda4c431bf50ff972
Umesh <umeshmanilal@gmail.com>
2016-01-06 20:41:18 -0600

    Loaded data in Bill of Materials screen

---
+++ MRP4u_Sample/MRP4u_Sample/Models/BillOfMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRP4ME_Sample.Models
+{
+    public class BillOfMaterial:BaseModel
+    {
+        private string id;
+        public string Id { get { return this.id; } set { SetProperty(ref id, value); } }
+
+        private string name;
+        public string Name { get { return this.name; } set { SetProperty(ref name, value); } }
+
+        private string unit;
+        public string Unit { get { return this.unit; } set { SetProperty(ref unit, value); } }
+
+        private decimal quantity;
+        public decimal Quantity { get { return this.quantity; } set { SetProperty(ref quantity, value); } }
+
+        private int level;
+        public int Level { get { return this.level; } set { SetProperty(ref level, value); } }
+
+        private string owner;
+        public string Owner { get { return this.owner; } set { SetProperty(ref owner, value); } }
+
+        private DateTime leadtime;
+        public DateTime LeadTime { get { return this.leadtime; } set { SetProperty(ref leadtime, value); } }
+    }
+}
--- MRP4u_Sample/MRP4u_Sample/ViewModels/BillOfMaterialPresentationModel.cs
+++ MRP4u_Sample/MRP4u_Sample/ViewModels/BillOfMaterialPresentationModel.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Regions;
+using MRP4ME_Sample.Models;
 using MRP4u_Sample.core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MRP4u_Sample.ViewModels
 {
@@ -14,6 +16,29 @@ namespace MRP4u_Sample.ViewModels
         public BillOfMaterialViewModel()
         {
             Title = "Bill Of Material";
+            this.InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            billOfMaterials = new List<BillOfMaterial>();
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+            billOfMaterials.Add(new BillOfMaterial() { Id = "PO101", Name = "A", Unit = "Piece", Quantity = 1, Level = 0, Owner = "Jennifer Martin", LeadTime = DateTime.Now });
+        }
+
+        private List<BillOfMaterial> billOfMaterials;
+        public List<BillOfMaterial> BillsOfMaterials
+        {
+            get
+            {
+                return billOfMaterials;
+            }
         }
     }
 }
