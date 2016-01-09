65d72a2a6a010e9d3e8ad48d282816bd4b7e6b81
Umesh <umeshmanilal@gmail.com>
2016-01-09 01:00:44 -0600

    added back the images which was removed after the conflict happened.

--- MRP4u_Sample/MRP4u_Sample/Models/ProductHierarchy.cs
+++ MRP4u_Sample/MRP4u_Sample/Models/ProductHierarchy.cs
@@ -17,15 +17,15 @@ namespace MRP4ME_Sample.Models
         private ProductHierarchy()
         {
             //populate data
-            list.Add(1, new Node { Id = 1, FirstName = "Item A - ", LastName = "(1)", ParentId = 1 });
-            list.Add(2, new Node { Id = 2, FirstName = "Item B - ", LastName = "(2)", ParentId = 1 });
-            list.Add(3, new Node { Id = 3, FirstName = "Item C - ", LastName = "(3)", ParentId = 1 });
-            list.Add(4, new Node { Id = 4, FirstName = "Item D - ", LastName = "(2)", ParentId = 2 });
-            list.Add(5, new Node { Id = 5, FirstName = "Item B - ", LastName = "(2)", ParentId = 2 });
-            list.Add(6, new Node { Id = 6, FirstName = "Item E - ", LastName = "(2)", ParentId = 3 });
-            list.Add(7, new Node { Id = 7, FirstName = "Item F - ", LastName = "(2)", ParentId = 3 });
-            list.Add(8, new Node { Id = 8, FirstName = "Item G - ", LastName = "(1)", ParentId = 7 });
-            list.Add(9, new Node { Id = 9, FirstName = "Item D - ", LastName = "(2)", ParentId = 7 });
+            list.Add(1, new Node { Id = 11, FirstName = "Item A - ", LastName = "(1)", ParentId = 11 });
+            list.Add(2, new Node { Id = 12, FirstName = "Item B - ", LastName = "(2)", ParentId = 11 });
+            list.Add(3, new Node { Id = 13, FirstName = "Item C - ", LastName = "(3)", ParentId = 11 });
+            list.Add(4, new Node { Id = 14, FirstName = "Item D - ", LastName = "(2)", ParentId = 12 });
+            list.Add(5, new Node { Id = 15, FirstName = "Item B - ", LastName = "(2)", ParentId = 12 });
+            list.Add(6, new Node { Id = 16, FirstName = "Item E - ", LastName = "(2)", ParentId = 13 });
+            list.Add(7, new Node { Id = 17, FirstName = "Item F - ", LastName = "(2)", ParentId = 13 });
+            list.Add(8, new Node { Id = 18, FirstName = "Item G - ", LastName = "(1)", ParentId = 17 });
+            list.Add(9, new Node { Id = 19, FirstName = "Item D - ", LastName = "(2)", ParentId = 17 });
         }
 
         internal static ProductHierarchy Instance()
